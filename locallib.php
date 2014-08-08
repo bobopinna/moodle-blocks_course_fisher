13b06424951eb30ac5f12c34e87becb331e17606
bobopinna <bobo@di.unipmn.it>
2014-08-08 16:24:45 +0200

    Modified to fit block setting fields

--- locallib.php
+++ locallib.php
@@ -30,19 +30,18 @@
    /**
     * Create a course, if not exits, and assign an editing teacher
     *
-    * @param string course_fullname The course name
-    * @param string course_id       The course code
-    * @param int    year            The academic year
-    * @param string teacher_id      The teacher id code
-    * @param array  categories      The academic year for this course
+    * @param string course_fullname  The course fullname
+    * @param string course_shortname The course shortname
+    * @param string course_id        The course code
+    * @param string teacher_id       The teacher id code
+    * @param array  categories       The categories from top category for this course
     *
     * @return object or null
     *
     **/
-    function block_course_fisher_create_course($course_fullname, $course_id, $year, $teacher_id, $extra = array()) {
+    function block_course_fisher_create_course($course_fullname, $course_shortname, $course_id, $teacher_id, $categories = array()) {
         global $DB, $CFG;
 
-        $shortnamemaxlength = 20;
         
         $newcourse = new stdClass();
 
@@ -56,16 +55,13 @@
 */
         $newcourse->startdate = time();
         
-        $endyear = $year+1;       
-        $newcourse->fullname = $year.'/'.$endyear.' '.$course_fullname;
-        $newcourse->shortname = textlib::substr($year.'-'.$course_id.' '.$course_fullname, 0, $shortnamemaxlength).'...';
+        $newcourse->fullname = $course_fullname;
+        $newcourse->shortname = $course_shortname;
         $newcourse->idnumber = $year.'-'.$course_id;
 
-        $newcourse->summary = get_string('course_summary','block_course_fisher').$course_fullname;
-
         $course = null;
-        if (!$oldcourse = $DB->get_record('course', array('shortname' => $newcourse->shortname))) {
-            $newcourse->category = block_course_fisher_create_categories($extra);
+        if (!$oldcourse = $DB->get_record('course', array('idnumber' => $newcourse->idnumber))) {
+            $newcourse->category = block_course_fisher_create_categories($categories);
             if (!$course = create_course($newcourse)) {
                 print_error("Error inserting a new course in the database!");
             }
@@ -75,7 +71,7 @@
 
         $editingteacherroleid = $DB->get_field('role', 'id', array('shortname' => 'editingteacher'));
 
-        if ($teacheruser = $DB->get_record('user', array('idnumber' => $teacher_id))) {
+        if ($teacheruser = $DB->get_record('user', array('id' => $teacher_id))) {
             // Set student role at course context
             $coursecontext = context_course::instance($course->id);
 
