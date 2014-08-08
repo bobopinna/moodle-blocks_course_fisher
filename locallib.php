80013d16dabf9cb578c25871210a70fbff6b4a94
bobopinna <bobo@di.unipmn.it>
2014-08-08 14:43:06 +0200

    added a function to include traslation for backend plugins

--- locallib.php
+++ locallib.php
@@ -97,3 +97,20 @@
 
         return $course;
     }
+
+    function block_course_fisher_backend_lang($lang, $blockstrings) {
+        global $CFG;
+
+        $backends = scandir($CFG->dirroot.'/blocks/course_fisher/backend');
+        foreach ($backends as $backend) {
+            if (file_exists($CFG->dirroot.'/blocks/course_fisher/backend/'.$backend.'/lang/'.$lang.'/coursefisherbackend_'.$backend.'.php')) {
+                $string = array();
+                include_once($CFG->dirroot.'/blocks/course_fisher/backend/'.$backend.'/lang/'.$lang.'/coursefisherbackend_'.$backend.'.php');
+                foreach ($string as $name => $translation) {
+error_log($name.' '.$translation);
+                    $blockstrings['backend_'.$backend.':'.$name] = $translation;
+                }
+            }
+        }
+        return $blockstrings;
+    }
