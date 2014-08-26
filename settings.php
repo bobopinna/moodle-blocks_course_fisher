1f381af8b7aa0c93ec018ae2af3c96608e196f83
dfantoma <dfantoma@users.noreply.github.com>
2014-08-26 11:07:22 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -64,6 +64,8 @@ if ($ADMIN->fulltree) {
     $settings->add(new admin_setting_configtext('block_course_fisher_course_fullname','Campo nome completo del corso','Usare campoCodice:campoDescrizione per associare il codice al nome', ''));
     
     $settings->add(new admin_setting_configtext('block_course_fisher_course_shortname','Campo nome breve del corso','Usare campoCodice:campoDescrizione per associare il codice al nome', ''));
+    
+    $settings->add(new admin_setting_configtext('block_course_fisher_course_helplink','Link alla pagina di help','inserire un link', ''));
 
     $choices = array();
     $choices[0] = get_string('viewcourse', 'block_course_fisher');
