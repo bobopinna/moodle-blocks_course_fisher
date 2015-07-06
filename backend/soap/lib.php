9acfe0a1037b24f8c50d1617d89ef336b1d33ca2
Roberto Pinna <bobo@di.unipmn.it>
2015-07-06 17:54:22 +0200

    Added SOAP backend

---
+++ backend/soap/lib.php
@@ -0,0 +1,115 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+class block_course_fisher_backend_soap extends block_course_fisher_backend {
+
+    public function __construct() {
+        $this->name = 'soap';
+    }
+
+    /**
+     * Reads informations for teacher courses using SOAP requests,
+     * then returns it in an array of objects.
+     *
+     * @return array
+     */
+    public function get_data($alldata=false) {
+        global $CFG;
+
+        $datas = array();
+
+        if (!empty($CFG->block_course_fisher_parameters)) {
+            $requestdata =  preg_replace_callback('/\[\%(\w+)\%\]/', 'parent::get_user_field', $CFG->block_course_fisher_parameters);
+            if ($alldata) {
+                $requestdata = preg_replace('/\[\%(\w+)\%\]/', '%', $CFG->block_course_fisher_parameters);
+            }
+            $requests = explode("\n", $requestdata);
+            if (!empty($requests)) {
+                foreach ($requests as $requestrow) {
+                    if ($datas !== false) {
+                        $request = new stdClass();
+                        $request->url = $CFG->block_course_fisher_locator;
+                        list($request->call, $request->parameter, $fieldlist) = explode('#', trim($requestrow));
+                        $request->fields = explode(',', $fieldlist);
+                        $datas = $this->soap_request($request, $datas);
+                    }
+                }
+            } else {
+                return false;
+            }
+        }
+
+        return $datas;
+    }
+
+    /**
+     * Sostituisce eventuali campi recuperati da query precedenti nei parametri da passare alla nuova query
+     *
+     * @string $parameter - stringa dei parametri [* e *] delimitano il nome di un campo presente in $data
+     * @object $data - dati recuperati da una precedente query
+     *
+     * return  @string
+     */
+    private function replace_parameter($parameter, $data) {
+        if (!empty($data)) {
+            foreach ($data as $fieldname => $value) {
+                $parameter = str_replace('[*'.$fieldname.'*]', $value, $parameter);
+            }   
+        }   
+        return $parameter;
+    }   
+
+    /**
+     * Effettua una o più richiestesoap e restituisce un'array con i dati recuperat
+     *
+     * @object $request - richiesta da effettuare che deve contenere i campi:
+     *                    @string url - url delle APIs
+     *                    @string call - nome della chiamata alle APIs
+     *                    @string parameter - elenco dei parametri da passare alla API nel formato nome=valore separati da ;
+     *                    @array  fields - elenco dei nomi dei campi da restituire
+     * @array  $datas - dati restituiti da una precedente query
+     *
+     * return  @array
+     */
+    private function soap_request($request, $datas=array()) {
+
+        $results = array();
+    
+        $data = current($datas);
+    
+        do {
+            $parameter = $this->replace_parameter($request->parameter, $data);
+            try {
+                $soap = new SoapClient($request->url);
+                $soapdata = $soap->fn_retrieve_xml_p($request->call, $parameter);
+                if (!empty($soapdata) && isset($soapdata['fn_retrieve_xml_pReturn']) && ($soapdata['fn_retrieve_xml_pReturn'] == 1) && !empty($soapdata['xml'])) {
+                    $soapxml = new SimpleXMLIterator($soapdata['xml']);
+                    $soapxml->rewind();
+                    for ($i=0; $i<count($soapxml->DataSet->Row); $i++) {
+                        $result = new stdClass();
+                        if (!empty($data)) {
+                            foreach ($data as $fieldname => $value) {
+                                $result->$fieldname = $value;
+                            }   
+                        }   
+                        foreach($request->fields as $fieldname) {
+                            if (isset($soapxml->DataSet->Row[$i]->$fieldname)) {
+                                $result->$fieldname = (string) $soapxml->DataSet->Row[$i]->$fieldname;
+                            } else {
+                                $result->$fieldname = ''; 
+                            }   
+                        }   
+                        $results[] = $result;
+                    }   
+                }   
+            } catch (SoapFault $e) {
+                return false;
+            }   
+            $data = next($datas);
+        } while ($data !== false);
+
+        return $results;
+    }
+
+}
