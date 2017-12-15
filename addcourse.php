0d142594f4ee8556f47d0c40f37441ebb1b117e0
Roberto Pinna <bobo@di.unipmn.it>
2017-12-15 11:44:37 +0100

    Fixed include syntax

--- addcourse.php
+++ addcourse.php
@@ -24,7 +24,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require(dirname(__FILE__) . '/../../config.php');
+require(__DIR__ . '/../../config.php');
 require_once('locallib.php');
 require_once('backendlib.php');
 require_once('preferences_form.php');
